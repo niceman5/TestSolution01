f6bcd959161bd37c73cd3c8ab75d5431f9c23ecd
niceman555 <niceman555@gmail.com>
2020-10-06 00:43:05 +0900

    exception1

---
+++ WFException01/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WFException01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnColorSave = new System.Windows.Forms.Button();
+            this.btnColorDelete = new System.Windows.Forms.Button();
+            this.lstColor = new System.Windows.Forms.ListBox();
+            this.pColor = new System.Windows.Forms.Panel();
+            this.tbarAlpha = new System.Windows.Forms.TrackBar();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.pBack = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarAlpha)).BeginInit();
+            this.pBack.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblColor);
+            this.groupBox1.Controls.Add(this.tbarAlpha);
+            this.groupBox1.Controls.Add(this.pColor);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 161);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color Select";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Color :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alpha :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " Text : ";
+            // 
+            // btnColorSave
+            // 
+            this.btnColorSave.Location = new System.Drawing.Point(140, 197);
+            this.btnColorSave.Name = "btnColorSave";
+            this.btnColorSave.Size = new System.Drawing.Size(103, 33);
+            this.btnColorSave.TabIndex = 1;
+            this.btnColorSave.Text = "저장";
+            this.btnColorSave.UseVisualStyleBackColor = true;
+            this.btnColorSave.Click += new System.EventHandler(this.btnColorSave_Click);
+            // 
+            // btnColorDelete
+            // 
+            this.btnColorDelete.Location = new System.Drawing.Point(279, 197);
+            this.btnColorDelete.Name = "btnColorDelete";
+            this.btnColorDelete.Size = new System.Drawing.Size(103, 33);
+            this.btnColorDelete.TabIndex = 2;
+            this.btnColorDelete.Text = "삭제";
+            this.btnColorDelete.UseVisualStyleBackColor = true;
+            this.btnColorDelete.Click += new System.EventHandler(this.btnColorDelete_Click);
+            // 
+            // lstColor
+            // 
+            this.lstColor.FormattingEnabled = true;
+            this.lstColor.ItemHeight = 17;
+            this.lstColor.Location = new System.Drawing.Point(12, 267);
+            this.lstColor.Name = "lstColor";
+            this.lstColor.Size = new System.Drawing.Size(370, 191);
+            this.lstColor.TabIndex = 3;
+            this.lstColor.SelectedIndexChanged += new System.EventHandler(this.lstColor_SelectedIndexChanged);
+            // 
+            // pColor
+            // 
+            this.pColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pColor.Location = new System.Drawing.Point(89, 35);
+            this.pColor.Name = "pColor";
+            this.pColor.Size = new System.Drawing.Size(20, 20);
+            this.pColor.TabIndex = 3;
+            this.pColor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pColor_MouseClick);
+            // 
+            // tbarAlpha
+            // 
+            this.tbarAlpha.Location = new System.Drawing.Point(75, 69);
+            this.tbarAlpha.Maximum = 255;
+            this.tbarAlpha.Name = "tbarAlpha";
+            this.tbarAlpha.Size = new System.Drawing.Size(104, 56);
+            this.tbarAlpha.TabIndex = 4;
+            this.tbarAlpha.Value = 255;
+            this.tbarAlpha.Scroll += new System.EventHandler(this.tbarAlpha_Scroll);
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(86, 121);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(16, 17);
+            this.lblColor.TabIndex = 5;
+            this.lblColor.Text = "-";
+            // 
+            // pBack
+            // 
+            this.pBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pBack.Controls.Add(this.panel5);
+            this.pBack.Controls.Add(this.panel4);
+            this.pBack.Controls.Add(this.panel1);
+            this.pBack.Controls.Add(this.panel3);
+            this.pBack.Controls.Add(this.panel2);
+            this.pBack.Location = new System.Drawing.Point(409, 12);
+            this.pBack.Name = "pBack";
+            this.pBack.Size = new System.Drawing.Size(460, 446);
+            this.pBack.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(64, 68);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(265, 110);
+            this.panel2.TabIndex = 0;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(234, 201);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(70, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(49, 201);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 125);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(234, 276);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(70, 40);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Location = new System.Drawing.Point(86, 360);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(286, 40);
+            this.panel5.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 486);
+            this.Controls.Add(this.pBack);
+            this.Controls.Add(this.lstColor);
+            this.Controls.Add(this.btnColorDelete);
+            this.Controls.Add(this.btnColorSave);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarAlpha)).EndInit();
+            this.pBack.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.TrackBar tbarAlpha;
+        private System.Windows.Forms.Panel pColor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnColorSave;
+        private System.Windows.Forms.Button btnColorDelete;
+        private System.Windows.Forms.ListBox lstColor;
+        private System.Windows.Forms.Panel pBack;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ WFException01/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFException01
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Color> dColor = new Dictionary<string, Color>();
+        Color oSelectColor = new Color();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pColor_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult dRet = colorDialog1.ShowDialog();
+
+            if (dRet == DialogResult.OK)
+            {
+                pColor.BackColor = colorDialog1.Color;
+            }
+
+            lblColor.Text = pColor.BackColor.ToString();
+        }
+
+        private void tbarAlpha_Scroll(object sender, EventArgs e)
+        {
+            pColor.BackColor = Color.FromArgb(tbarAlpha.Value, pColor.BackColor);
+            lblColor.Text = pColor.BackColor.ToString();
+        }
+
+        private void btnColorSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Color oColor = pColor.BackColor;
+                dColor.Add(oColor.ToString(), oColor);
+
+                ListBoxRefres();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void ListBoxRefres()
+        {
+            try
+            {
+                lstColor.Items.Clear();
+
+                foreach (string pKey in dColor.Keys)
+                {
+                    lstColor.Items.Add(pKey);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void btnColorDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstColor.SelectedItem != null && dColor.ContainsKey(lstColor.SelectedItem.ToString()))
+                {
+                    dColor.Remove(lstColor.SelectedItem.ToString());
+                    ListBoxRefres();
+                }
+                else
+                {
+                    MessageBox.Show("삭제할 item이 없거나 키가 없습니다.");
+                }
+
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void lstColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //선택값 변경시...
+            oSelectColor = dColor[(lstColor.SelectedItem.ToString()];
+            pColor.BackColor = oSelectColor;
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
