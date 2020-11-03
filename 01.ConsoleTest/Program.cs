36c0f2681b18b88b30f3bca02e079737f11f739c
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2020-11-03 18:02:16 +0900

    11111

---
+++ 01.ConsoleTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Npgg;
+
+namespace _01.ConsoleTest
+{
+    class Program
+    {
+
+        class Item
+        {
+            public string Name { get; set; }
+            public Rarity Rarity { get; set; }
+            public string Slot { get; set; }
+        }
+
+        enum Rarity
+        {
+            Normal,
+            Magic,
+            Unique,
+            Legendary,
+        }
+
+        static void Main(string[] args)
+        {
+            var items = new[]
+            {
+                new Item(){ Name= "Leoric's Crown", Rarity = Rarity.Normal, Slot ="Helm"},
+                new Item(){ Name= "Thunderfury", Rarity = Rarity.Unique, Slot ="One Handed Weapon"},
+                new Item(){ Name= "할배검 the grandfather", Rarity = Rarity.Legendary, Slot ="Two Handed Weapon"},
+                new Item(){ Name= "WINDFORCE", Rarity = Rarity.Magic, Slot ="양손무기"},
+            };
+
+            ConsoleTable.Write(items, item => item.Rarity switch
+            {
+                Rarity.Magic => ConsoleColor.DarkCyan,
+                Rarity.Unique => ConsoleColor.DarkMagenta,
+                Rarity.Legendary => ConsoleColor.DarkYellow,
+                _ => ConsoleColor.White
+            });
+
+            ConsoleTable.TableColor = ConsoleColor.Red;
+            ConsoleTable.ColumnColor = ConsoleColor.Cyan;
+            ConsoleTable.RowColor = ConsoleColor.White;
+
+            ConsoleTable.WriteSingle(items[0]);
+        }
+    }
+}
