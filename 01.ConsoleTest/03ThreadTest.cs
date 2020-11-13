820477ace88ead10faf293a6edf6e005cd1f90ef
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2020-11-13 19:06:30 +0900

    111111

--- 01.ConsoleTest/03ThreadTest.cs
+++ 01.ConsoleTest/03ThreadTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
+using Serilog;
 
 namespace _01.ConsoleTest
 {
@@ -27,14 +28,19 @@ namespace _01.ConsoleTest
 
         public void Working()
         {
-            while (true)
-            {
-                string path = "C:\\Logs\\sample.txt";
+            string path = "C:\\Logs\\sample.txt";
+            Log.Logger = new LoggerConfiguration()
+                              .MinimumLevel.Debug()
+                              .WriteTo.Console()
+                              .WriteTo.File(path + "\\log-{Date}.log",
+                                  outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                              .CreateLogger();
 
-                using (StreamWriter w = new StreamWriter(path, true))
-                {
-                    w.WriteLine($"window service is call on {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss tt")}");
-                }
+            while (true)            
+            {
+
+                Log.Information("Log Write");
+                
                 Thread.Sleep(1000 * 5);     //5초간
             }
         }
--- 01.ConsoleTest/Program.cs
+++ 01.ConsoleTest/Program.cs
@@ -11,17 +11,17 @@ namespace _01.ConsoleTest
 
         static void Main(string[] args)
         {
-            //칼라 콘솔 출력
-            var ctt = new ConsoleTableTest();
-            ctt.ConsoleTablePrint();
+            ////칼라 콘솔 출력
+            //var ctt = new ConsoleTableTest();
+            //ctt.ConsoleTablePrint();
 
-            // App.config설정
-            var at = new AppTest();
-            at.AppSetPrint();
+            //// App.config설정
+            //var at = new AppTest();
+            //at.AppSetPrint();
 
             // thread사용
-            //var tt = new ThreadTest();
-            //tt.ThreadPrint();
+            var tt = new ThreadTest();
+            tt.ThreadPrint();
 
             //var dt = new _04DapperTest();
             //dt.Execute();
