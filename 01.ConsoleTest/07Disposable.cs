3d51ffde2ca96abfd2eb62a8c36f50e0ec50ccc8
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2020-12-02 22:31:12 +0900

    1111

---
+++ 01.ConsoleTest/07Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _01.ConsoleTest
+{
+    /// <summary>
+    /// using으로 사용할수 있게 만듬
+    /// : IDisposable를 상속받고 Dispose()를 반드시 구현해야 함.
+    /// 이걸 using에 사용하면 반드시 닫힘.
+    /// </summary>
+    class _07Disposable : IDisposable
+    {
+        private TextWriter writer = null;
+
+        public void Create() 
+        {
+            writer = File.CreateText("sample.txt");
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public void Write() 
+        {
+            throw new ApplicationException("Sample Exception");
+        }
+        public void Dispose()
+        {
+            if (writer != null) writer.Close();
+        }
+    }
+}
