edcff00e6a03de6cc9e9be992c3a6dc11914870e
김태훈 <niceman555@gmail.com>
2020-12-09 23:30:00 +0900

    metro

---
+++ 46MetroUITest/Form1.cs
@@ -0,0 +1,21 @@
+﻿using MetroSet_UI.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _46MetroUITest
+{
+    public partial class Form1 : MetroSetForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
