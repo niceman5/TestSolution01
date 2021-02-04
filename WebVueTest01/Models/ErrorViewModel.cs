f115ee0b5954398e6185222eecfdc94f64aa54dc
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2021-02-04 19:36:31 +0900

    vue관련

---
+++ WebVueTest01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebVueTest01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
