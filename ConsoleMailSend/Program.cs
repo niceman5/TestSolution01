1d5b56d4a02d90ab01bb447b3de39a26f85b893c
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2021-02-27 13:09:45 +0900

    11111

--- ConsoleMailSend/Program.cs
+++ ConsoleMailSend/Program.cs
@@ -12,13 +12,14 @@ namespace ConsoleMailSend
             client.EnableSsl = true;                    //SSL 사용한다.
             client.DeliveryMethod = SmtpDeliveryMethod.Network; // 이걸해야만 인증을 받는다.
             client.Credentials = new System.Net.NetworkCredential("niceman555", "gustnr35!A");
+            //client.Credentials = new System.Net.NetworkCredential("kth", "gustnr35");           //회사메일
 
             /*
              * 구글 계정에서 보안수준이 낮은 앱의 엑세스 권한을 허용으로 해야 함
              * */
 
-            MailAddress from = new MailAddress("niceman555@gmila.com","김태훈", System.Text.Encoding.UTF8);
-            MailAddress to = new MailAddress("niceman5@naver.com");
+            MailAddress from = new MailAddress("niceman555@gmail.com","김태훈", System.Text.Encoding.UTF8);
+            MailAddress to = new MailAddress("kth@u-crm.com");
 
             MailMessage message = new MailMessage(from, to);
 
@@ -30,12 +31,15 @@ namespace ConsoleMailSend
             message.SubjectEncoding = System.Text.Encoding.UTF8;
 
             //파일 첨부
-            var attch1 = new Attachment(@"C:\00.Dev\temp\ChartTest.xlsx");
-            var attch2 = new Attachment(@"C:\00.Dev\temp\test.xlsx");
-            var attch3 = new Attachment(@"C:\00.Dev\temp\HyprtLink_Font.xlsx");
+            //var attch1 = new Attachment(@"C:\00.Dev\temp\ChartTest.xlsx");
+            //var attch2 = new Attachment(@"C:\00.Dev\temp\test.xlsx");
+            //var attch3 = new Attachment(@"C:\00.Dev\temp\HyprtLink_Font.xlsx");
+            //message.Attachments.Add(attch1);
+            //message.Attachments.Add(attch2);
+            //message.Attachments.Add(attch3);
+
+            var attch1 = new Attachment(@"D:\00.Dev\temp\코인적립신청리스트_2021-02.xlsx");
             message.Attachments.Add(attch1);
-            message.Attachments.Add(attch2);
-            message.Attachments.Add(attch3);
 
             try
             {
